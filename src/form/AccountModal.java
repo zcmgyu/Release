fcf22ddb286c7e0b472967d946991c9e47e37cc7
zcmgyu <zcmgyu@10.211.55.3>
2015-11-02 19:57:42 +0700

    Moi sua cai string

--- src/form/AccountModal.java
+++ src/form/AccountModal.java
@@ -7,7 +7,7 @@ package form;
 public class AccountModal {
     private String username;
     private int role;
-    private int test;
+    private String daylatest;
     public AccountModal() {
     }
 
