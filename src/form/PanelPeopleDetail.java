32a6eef525538cff079b3d054e6ddcfb90018769
zcmgyu <zcmgyu@10.211.55.3>
2015-11-16 21:32:20 +0700

    1h40 Ngay 15/11/2015

--- src/form/PanelPeopleDetail.java
+++ src/form/PanelPeopleDetail.java
@@ -8,6 +8,7 @@ package form;
 import java.awt.Component;
 import java.awt.Font;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -16,6 +17,7 @@ import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -33,6 +35,8 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
     /**
      * Creates new form ResidentManager
      */
+    Vector vColumn;
+    Vector vData;
     public PanelPeopleDetail() {
         initComponents();
         initPeopleDetail(null);
@@ -84,8 +88,8 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
     }
 
     final void initPeopleDetail(String sql) {
-        Vector vColumn = new Vector();
-        Vector vData = new Vector();
+        vColumn = new Vector();
+        vData = new Vector();
         if (sql == null) {
             sql = "select h.ID, h.Name [Họ và tên], h.Birthday [Ngày sinh], h.Gender [Giới tính],"
                     + " h.Birthplace [Nơi sinh], h.[Native Country] [Dân tộc], h.Nation [Quốc tịch],"
@@ -105,7 +109,12 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
             while (rs.next()) {
                 Vector vRow = new Vector();
                 for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-                    vRow.add(rs.getString(i));
+                    if(i == 4) {
+                        vRow.add((rs.getInt(4)==1)?"Nam":"Nữ");
+                    } 
+                    else{
+                        vRow.add(rs.getString(i));
+                    }
                 }
                 vData.add(vRow);
             }
@@ -113,7 +122,6 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
         } catch (SQLException ex) {
             Logger.getLogger(ApartmentManagementForm.class.getName()).log(Level.SEVERE, null, ex);
         }
-
     }
 
     /**
@@ -126,20 +134,74 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
     private void initComponents() {
 
         pnlFooter = new javax.swing.JPanel();
+        btnAdd = new org.jdesktop.swingx.JXButton();
+        btnReplace = new org.jdesktop.swingx.JXButton();
+        btnDelete = new org.jdesktop.swingx.JXButton();
+        btnDeltail = new org.jdesktop.swingx.JXButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblPeopleDetail = new org.jdesktop.swingx.JXTable();
 
         pnlFooter.setBackground(new java.awt.Color(153, 153, 255));
 
+        btnAdd.setText(" Thêm ");
+        btnAdd.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAddActionPerformed(evt);
+            }
+        });
+
+        btnReplace.setText("  Sửa  ");
+        btnReplace.setMaximumSize(new java.awt.Dimension(75, 36));
+        btnReplace.setMinimumSize(new java.awt.Dimension(75, 36));
+        btnReplace.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnReplaceActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setText("  Xóa  ");
+        btnDelete.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeleteActionPerformed(evt);
+            }
+        });
+
+        btnDeltail.setText("Chi tiết");
+        btnDeltail.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnDeltailActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout pnlFooterLayout = new javax.swing.GroupLayout(pnlFooter);
         pnlFooter.setLayout(pnlFooterLayout);
         pnlFooterLayout.setHorizontalGroup(
             pnlFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(pnlFooterLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(btnReplace, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(4, 4, 4)
+                .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnDeltail, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         pnlFooterLayout.setVerticalGroup(
             pnlFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 82, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlFooterLayout.createSequentialGroup()
+                .addGroup(pnlFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(pnlFooterLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(pnlFooterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnDelete, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnDeltail, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnReplace, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(pnlFooterLayout.createSequentialGroup()
+                        .addGap(35, 35, 35)
+                        .addComponent(btnAdd, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
         );
 
         jScrollPane1.setBackground(new java.awt.Color(255, 255, 255));
@@ -174,14 +236,89 @@ public final class PanelPeopleDetail extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 249, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(pnlFooter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
+        
+        DialogAddHuman abc = new DialogAddHuman(null, true);
+        abc.setLocationRelativeTo(this);
+        abc.setVisible(true);
+    }//GEN-LAST:event_btnAddActionPerformed
+
+    private void btnReplaceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnReplaceActionPerformed
+        int rowId = tblPeopleDetail.getSelectedRow();
+        if(rowId>0){
+            Vector vRow = (Vector) vData.get(rowId);
+            ReplaceHumanForm replaceHumanForm =  new ReplaceHumanForm(null, true, this,vRow);
+            replaceHumanForm.setLocationRelativeTo(this);
+            replaceHumanForm.setVisible(true);
+            ShareData.getInstance().getPpd().initCellAlign();
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Vui lòng chọn người để chỉnh sửa", "Lỗi",JOptionPane.ERROR_MESSAGE,new javax.swing.ImageIcon(getClass().getResource("/image/error.png")));
+        }
+
+    }//GEN-LAST:event_btnReplaceActionPerformed
+
+    private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
+        int rowId = tblPeopleDetail.getSelectedRow();
+        System.out.println("ROWID: " + rowId);
+        if(rowId>0){
+            int response = JOptionPane.showConfirmDialog(null, "Do you want to delete?", "Confirm",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (response == JOptionPane.NO_OPTION) {
+                
+            } else if (response == JOptionPane.YES_OPTION) {
+                System.out.println("LONG");
+                System.out.println("VData: " + vData);
+                                System.out.println("VaData: " + vData.get(rowId));
+
+                Vector vRow = (Vector) vData.get(rowId);
+                String sql = "delete from tblHuman where id="+Integer.parseInt(vRow.get(0).toString());
+                System.out.println(sql);
+                try (Connection cn = Tools.getConn();
+                        PreparedStatement pst = cn.prepareStatement(sql);) {
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(this, "Delete successful");
+                    ShareData.getInstance().getPpd().initPeopleDetail(null);
+                    ShareData.getInstance().getPpd().initWidthTable();
+                    ShareData.getInstance().getPpd().initCellAlign();
+                    //
+                    
+                } catch (SQLException ex) {
+                    Logger.getLogger(ApartmentManagementForm.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } else if (response == JOptionPane.CLOSED_OPTION) {
+                System.out.println("JOptionPane closed");
+            }
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Vui lòng chọn người để xóa", "Lỗi",JOptionPane.ERROR_MESSAGE,new javax.swing.ImageIcon(getClass().getResource("/image/alert.png")));
+        }
+    }//GEN-LAST:event_btnDeleteActionPerformed
+
+    private void btnDeltailActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeltailActionPerformed
+        int rowId = tblPeopleDetail.getSelectedRow();
+        if(rowId>0){
+            Vector vRow = (Vector) vData.get(rowId);
+            DialogDetailHuman detailHuman =  new DialogDetailHuman(null, true, Integer.parseInt(vRow.get(0).toString()));
+            detailHuman.setLocationRelativeTo(this);
+            detailHuman.setVisible(true);
+        }
+        else {
+            JOptionPane.showMessageDialog(null, "Vui lòng chọn người để hiển thị", "Lỗi",JOptionPane.ERROR_MESSAGE,new javax.swing.ImageIcon(getClass().getResource("/image/alert.png")));
+        }
+    }//GEN-LAST:event_btnDeltailActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private org.jdesktop.swingx.JXButton btnAdd;
+    private org.jdesktop.swingx.JXButton btnDelete;
+    private org.jdesktop.swingx.JXButton btnDeltail;
+    private org.jdesktop.swingx.JXButton btnReplace;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JPanel pnlFooter;
     private org.jdesktop.swingx.JXTable tblPeopleDetail;
